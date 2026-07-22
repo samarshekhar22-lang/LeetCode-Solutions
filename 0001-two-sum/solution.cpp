1b28f1bb4359ab711292f549d77840bea29bb981
samarshekhar22-lang <samarshekhar22@gmail.com>
2026-07-22 12:02:36 +0000

    Sync LeetCode submission Runtime - 45 ms (14.61%), Memory - 14.2 MB (64.22%)

---
+++ 0001-two-sum/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v1;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]+nums[j]==target){
+                    v1.push_back(i);
+                    v1.push_back(j);
+                    return v1;
+                }
+            }
+        }
+        return v1;
+    }
+};
