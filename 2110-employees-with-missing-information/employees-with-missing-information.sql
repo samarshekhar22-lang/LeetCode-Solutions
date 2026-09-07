1b3d682839883145440823f519052693f8ef2672
samarshekhar22-lang <samarshekhar22@gmail.com>
2026-09-07 16:13:11 +0530

    Time: 504 ms (98.56%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,11 +1,13 @@
-SELECT employee_id 
-FROM Employees 
-WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+SELECT e.employee_id 
+FROM Employees e
+LEFT JOIN Salaries s ON e.employee_id = s.employee_id
+WHERE s.salary IS NULL
 
 UNION
 
-SELECT employee_id 
-FROM Salaries 
-WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+SELECT s.employee_id 
+FROM Salaries s
+LEFT JOIN Employees e ON s.employee_id = e.employee_id
+WHERE e.name IS NULL
 
 ORDER BY employee_id;
